71df9ee1c187e689a3d42fee7888772d1736642a
DESKTOP-FQBJS4T\Hacky_Shaggy <karthick.psgtec12@gmail.com>
2018-08-20 03:07:46 +0530

    Basic Git api fetch call implemented

--- src/components/issueDescribePage/IssueDescribePage.js
+++ src/components/issueDescribePage/IssueDescribePage.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
 
 class issueDescripePage extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/KarthickAthlet")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // this.setState({
+          //   isLoaded: true,
+          //   items: result.items
+          // });
+          console.log("success");
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          // this.setState({
+          //   isLoaded: true,
+          //   error
+          // });
+          console.log("failed");
+        }
+      );
+  }
+
   render() {
     return (<div>
               <h1>Issue Describe Page</h1> 
